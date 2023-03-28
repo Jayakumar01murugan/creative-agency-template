62364609027b74c9f68be4072fed3dc9ec301d00
Jaya Kumar Murugan <jayakumarm725@gmail.com>
2023-03-28 13:36:06 +0530

    [Jay] Footer responsive fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,22 +11,22 @@ const Footer = () => {
                         <img className='footer-logo' src={logo} alt="logo" />
                     </div>
                     <div className='row_column-2 column'>
-                        <h6>Menu</h6>
+                        <h6>MENU</h6>
                         <p className='footer-text'>
-                            <a className='footer-link' href='#about'>About</a><br />
-                            <a className='footer-link' href='#portfolio'>Portfolio</a><br />
-                            <a className='footer-link' href='#services'>Services</a><br />
-                            <a className='footer-link' href='#contact'>Contact</a><br />
+                            <a className='footer-link' href='#about'>ABOUT</a><br />
+                            <a className='footer-link' href='#portfolio'>PORTFOLIO</a><br />
+                            <a className='footer-link' href='#services'>SERVICES</a><br />
+                            <a className='footer-link' href='#contact'>CONTACT</a><br />
                         </p>
                     </div>
                     <div className='row_column-3 column'>
-                        <h6>Address</h6>
+                        <h6>ADDRESS</h6>
                         <p className='footer-text'>
                             Suite #913<br />Corporation Building<br />Tibb Street<br />Manchester<br />M1 1TA
                         </p>
                     </div>
                     <div className='row_column-4 column'>
-                        <h6>Contact</h6>
+                        <h6>CONTACT</h6>
                         <p className='footer-text' >
                             E: info(at)youragency.com<br />T: +44 800 123 456<br />F: +44 800 123 400
                         </p>
