6a810b34610fd43ce61aceeb934c09d432b9b314
Jaya Kumar Murugan <jayakumarm725@gmail.com>
2023-03-29 16:29:30 +0530

    [Jay] Header section responsive fixed for text and images

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,24 +13,24 @@ const Header = () => {
     <header className='header-section'>
       <Slide>
         <div className="each-slide-effect">
-          <div style={{ 'backgroundImage': `url(${slider1})` }}>
-            <div className='container'>
+          <div className='slide1' style={{ 'backgroundImage': `url(${slider1})` }}>
+            <div className='slide-container'>
               <h1>We can change <br /> our digital world</h1>
               <h4>Bold enough to blow a hole in your next marketing campaign.</h4>
             </div>
           </div>
         </div>
         <div className="each-slide-effect">
-          <div style={{ 'backgroundImage': `url(${slider2})` }}>
-            <div className='container'>
+          <div className='slide2' style={{ 'backgroundImage': `url(${slider2})` }}>
+            <div className='slide-container'>
               <h1>You need<br />a good partner</h1>
               <h4>Let us help you realise your true potential</h4>
             </div>
           </div>
         </div>
         <div className="each-slide-effect">
-          <div style={{ 'backgroundImage': `url(${slider3})` }}>
-            <div className='container'>
+          <div className='slide3' style={{ 'backgroundImage': `url(${slider3})` }}>
+            <div className='slide-container'>
               <h1>Shoot for<br />the hoop!</h1>
               <h4>Aiming low is not an option for us</h4>
             </div>
